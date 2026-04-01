30926c7af33b829d2116225ee566d8d70967c6c3
Falianzzzz <tommasofalia10@gmail.com>
2026-04-01 10:42:17 +0200

    css fatto poco js fatto niente

---
+++ monumenti.js
@@ -0,0 +1,9 @@
+/*Crea il file monumenti.js dove sono presenti due funzioni.
+La funzione aggiungiMonumenti(mappa) che prende in ingresso il riferimento della mappa e aggiunge determinati monumenti di Roma sulla mappa utilizzando dei marker.
+I monumenti sono presi da un array di oggetti con latitudine e longitudine definiti in maniera globale. Farsi aiutare dall'intelligenza artificiale.
+Inoltre è presente la funzione mostraStatisticheMonumenti() che mostra un alert con il numero di monumenti mostrati sulla mappa (cioè la dimensione dell'array di oggetti)
+ATTENZIONE: Nel file monumenti.js è presente solo l'array di oggetti e le due funzioni. Nulla di più. */
+
+function aggiungiMonumenti(mappa) {
+    
+}
